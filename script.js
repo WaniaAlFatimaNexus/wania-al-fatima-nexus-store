462fe2524d2a04c09a1fede0d9fc44db50508143
WaniaAlFatimaNexus <mareebasif0191@gmail.com>
2026-08-06 20:18:05 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Wania Al Fatima Nexus Website Script
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const orderButton = document.querySelector("button");
+
+    orderButton.addEventListener("click", function () {
+        window.open(
+            "https://wa.me/923000000000?text=Assalamualaikum%20I%20want%20to%20order%20Wania%20Al%20Fatima%20Nexus%20products",
+            "_blank"
+        );
+    });
+
+});
